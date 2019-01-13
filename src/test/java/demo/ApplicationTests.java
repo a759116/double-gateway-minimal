80d836601e09b981e3529ed2573af185b436a512
Rudra Panda <rudra.panda@optum.com>
2019-01-12 20:10:23 -0600

    preparing for github

---
+++ src/test/java/demo/ApplicationTests.java
@@ -0,0 +1,30 @@
+package demo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class ApplicationTests {
+
+	@org.springframework.boot.web.server.LocalServerPort
+	private int port;
+
+	private TestRestTemplate template = new TestRestTemplate();
+
+	@Test
+	public void homePageLoads() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:"
+				+ port + "/", String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+
+}
